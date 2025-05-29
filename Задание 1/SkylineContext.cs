efc7ada224d98b47811521b7a14ceec1404309ef
nikitaarutiunian <vishnevetskinikita@gmail.com>
2025-05-29 21:46:07 +0300

    Add files via upload

---
+++ Задание 1/SkylineContext.cs
@@ -0,0 +1,21 @@
+﻿using LAB3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LAB3
+{
+    public class SkylineContext : DbContext
+    {
+        public DbSet<Star> Stars { get; set; }
+        public DbSet<Planet> Planets { get; set; }
+        public DbSet<Constellation> Constellations { get; set; }
+        public SkylineContext(DbContextOptions options) => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlite("Data Source=app.db");
+            }
+        }
+    }
+}
